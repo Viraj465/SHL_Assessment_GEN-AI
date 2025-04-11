7fd578be46ca2521e0a0c3f1975b996b414c206c
Viraj465 <sawantviraj465@gmail.com>
2025-04-11 15:02:08 +0530

    added app and route in root

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-import sys
-from pathlib import Path
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from route import router
+# from route import router
 
-sys.path.append(str(Path(__file__).resolve().parent / "app" / "api"))
-from app.app import app
+app = FastAPI(
+    title="SHL Assessment Recommendation System API",
+    description="API for recommending SHL assessments",
+    version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router, prefix="/api")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, Welcome to SHL Assessment Recommender API"}
 
 if __name__ == "__main__":
     import uvicorn
---
+++ route.py
@@ -0,0 +1,26 @@
+from fastapi import HTTPException, APIRouter
+from pydantic import BaseModel
+from app.api.services.llmService import get_recommendations
+
+router = APIRouter()
+
+class Query(BaseModel):
+    question: str
+    history: list = []
+
+class RecommendationResponse(BaseModel):
+    answer: str
+    recommendations: list
+
+@router.post("/Hello")
+async def hello_world():
+    return {"message": "Hello, World!"}
+@router.post("/recommendations/", response_model=RecommendationResponse)
+async def get_recommendations(query: Query):
+    try:
+        response = get_recommendations(query.question, query.history)
+        return {"answer":response['answer'],
+                "recommendations":response['recommendations']}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
